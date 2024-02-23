746d4176437c4360dd12ccdabd376a57561f3084
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-02-23 10:58:39 -0600

    Updated login view

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,37 @@
 "use client";
 
+import variables from "@/app/page.module.scss";
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Login() {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <div className="position-absolute w-100 vh-100 start-0 top-0 d-flex justify-content-center align-items-center">
-      <button onClick={loginWithRedirect} className="btn btn-primary">
-        Log in
-      </button>
+    <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+      <div className="card p-3">
+        <h1 className="text-center">
+          <span style={{ color: variables.liverpoolColor }}>Liverpool</span> Technical Test
+        </h1>
+        <h2 className="text-center">React Front End Developer</h2>
+        <div className="row  row-cols-2 row-cols-sm-3 pt-3 pb-3 justify-content-center">
+          <img className="col" src="/react.png" />
+        </div>
+        <div className="row p-3 justify-content-center">
+          <div className="col-auto">
+            <button
+              onClick={loginWithRedirect}
+              style={{
+                backgroundColor: variables.liverpoolColor,
+                borderColor: variables.liverpoolColor
+              }}
+              className="btn btn-primary"
+            >
+              <i className="bi bi-door-open pe-1"></i>Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
